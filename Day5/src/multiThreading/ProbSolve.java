1b6fa969b95a6414bfead9d38c0e583b720eba58
v-ajithkumar <ajithkumarjw707@gmail.com>
2026-06-25 16:22:34 +0530

    learned about multithreading and synchronization

---
+++ Day5/src/multiThreading/ProbSolve.java
@@ -0,0 +1,25 @@
+package multiThreading;
+
+public class ProbSolve {
+	public static void main(String[] args) throws InterruptedException {
+		//Print numbers using two threads.
+		Thread t1 = new Thread (new Runnable () {
+			public void run() {
+				for(int i=1 ; i<=5 ; i++) {
+					System.out.println("thread 1: " + i);
+				}
+			}
+		});
+		Thread t2 = new Thread (new Runnable () {
+			public void run() {
+				for(int i=1 ; i<=5 ; i++) {
+					System.out.println("thread 1: " + i);
+				}
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+	}
+}
---
+++ Day5/src/multiThreading/SynchronizedEg.java
@@ -0,0 +1,40 @@
+package multiThreading;
+
+class CakeCounter {
+	int cakeCount;
+
+	public  void increment() {
+		cakeCount++;
+	}
+
+}
+
+public class SynchronizedEg {
+	public static void main(String[] args) throws InterruptedException {
+		CakeCounter count = new CakeCounter();
+		Thread team1 = new Thread(new Runnable()
+		{
+			public void run() {
+				for(int i=0 ; i<1000 ; i++) {
+					count.increment();
+				}
+			}
+		});
+		
+		Thread team2 = new Thread(new Runnable()
+		{
+			public void run() {
+				for(int i=0 ; i<1000 ; i++) {
+					count.increment();
+				}
+			}
+		});
+		
+		
+		team1.start();
+		team2.start();
+		team1.join();
+		team2.join();
+		System.out.println(count.cakeCount);
+	}
+}
