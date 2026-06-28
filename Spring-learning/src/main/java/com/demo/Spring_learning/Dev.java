e179901a4fc73b3da8729b017291463070739310
v-ajithkumar <ajithkumarjw707@gmail.com>
2026-06-28 18:10:12 +0530

    Day-8  learned about spring framework and concepts IOc and DI

---
+++ Spring-learning/src/main/java/com/demo/Spring_learning/Dev.java
@@ -0,0 +1,10 @@
+package com.demo.Spring_learning;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Working on a project..........");
+    }
+}
---
+++ Spring-learning/src/main/java/com/demo/Spring_learning/SpringLearningApplication.java
@@ -0,0 +1,17 @@
+package com.demo.Spring_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringLearningApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context;
+		context = SpringApplication.run(SpringLearningApplication.class, args);
+		Dev d1 = context.getBean(Dev.class);
+		d1.build();
+	}
+
+}
