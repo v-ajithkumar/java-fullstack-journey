db4439060998070662aef9e7439272fa0cdb044b
v-ajithkumar <ajithkumarjw707@gmail.com>
2026-07-21 08:40:18 +0530

    added bean scope

---
+++ Spring-learning/src/main/java/com/demo/Spring_learning/Day10/ScopeOfBean.java
@@ -0,0 +1,17 @@
+package com.demo.Spring_learning.Day10;
+
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
+
+//@Component
+//@Scope("singleton")
+//@Scope("prototype")
+//@RequestScope
+//@SessionScope
+//@ApplicationScope
+public class ScopeOfBean {
+}
