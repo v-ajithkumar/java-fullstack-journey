34bf2431bb83c719bd8003a4c7251fd38e2bf30e
v-ajithkumar <ajithkumarjw707@gmail.com>
2026-07-26 16:55:48 +0530

    refactor this codes

--- Day7-js/this.js
+++ Day7-js/this.js
@@ -1,21 +1,27 @@
-const student = {
-        name : "John",
-        greet() {
-            console.log(this.name);
-        }
-    }
-    student.greet();
+// const student = {
+//         name : "John",
+//         greet() {
+//             console.log(this.name);
+//         }
+//     }
+//     student.greet();
 
-var name = "Global"
-    const student2 = {
-        name : "mark",
-        show : function(){
-            function call(){
-                const greet= () => {
+var name = "Global";
+
+const student2 = {
+    name : "Mark",
+
+    show: function () {
+
+        const call = () => {
+            const greet = () => {
                 console.log(this.name);
-            }
+            };
             greet();
-            }
-        }
+        };
+
+        call();
     }
-student2.show()
+};
+
+student2.show();
